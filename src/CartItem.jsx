272ab7c84710a82ac7db9adeb2715593b78a33bd
Sini Sajin <SINI@MICROCENTERGULF.COM>
2025-04-03 02:09:46 -0400

    Adding Paradise Nursery Shopping Application

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,27 +9,40 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+  let total = 0;
+
+    cart.forEach(item => {
+      const itemCost = parseFloat(item.cost.substring(1)); // Convert cost from "$10.00" to 10.00
+      total += item.quantity * itemCost; // Multiply by quantity
+    });
+
+    return total.toFixed(2);
   };
 
   const handleContinueShopping = (e) => {
-   
+    onContinueShopping(e)
   };
 
-
+ const handleCheckoutShopping = (e) => {
+    window.alert('Coming Soon');
+  };
 
   const handleIncrement = (item) => {
+     dispatch(updateQuantity({ name: item.name, quantity: item.quantity + 1 }));
   };
 
   const handleDecrement = (item) => {
-   
+    dispatch(updateQuantity({ name: item.name, quantity: item.quantity - 1 }));
   };
 
   const handleRemove = (item) => {
+    dispatch(removeItem(item.name));
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+     const itemCost = parseFloat(item.cost.substring(1)); // Convert "$10.00" to 10.00
+    return (item.quantity * itemCost).toFixed(2);
   };
 
   return (
@@ -57,7 +70,7 @@ const CartItem = ({ onContinueShopping }) => {
       <div className="continue_shopping_btn">
         <button className="get-started-button" onClick={(e) => handleContinueShopping(e)}>Continue Shopping</button>
         <br />
-        <button className="get-started-button1">Checkout</button>
+        <button className="get-started-button1"  onClick={(e) => handleCheckoutShopping(e)}>Checkout</button>
       </div>
     </div>
   );
